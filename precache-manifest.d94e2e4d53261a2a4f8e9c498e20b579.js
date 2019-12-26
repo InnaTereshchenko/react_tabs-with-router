7702037b0e5da2bd015e097cdf30977f0fc927ae
Inna <inna.tereshchenko1@gmail.com>
2019-12-26 14:50:19 +0200

    Updates

---
+++ precache-manifest.d94e2e4d53261a2a4f8e9c498e20b579.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30d954b74e0cc2771296a8ad538e872",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "26c9bcffd31d1cfad7c4",
+    "url": "/react_tabs-with-router/static/css/main.cb3eaede.chunk.css"
+  },
+  {
+    "revision": "d685dbebd803ee964aca",
+    "url": "/react_tabs-with-router/static/js/2.5918ce14.chunk.js"
+  },
+  {
+    "revision": "26c9bcffd31d1cfad7c4",
+    "url": "/react_tabs-with-router/static/js/main.9e5ef736.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
